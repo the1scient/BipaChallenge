d34b34c016a4af8207bdc97cea3b25a169cc8212
the1scient <guifabbri04@hotmail.com>
2026-01-08 21:34:55 -0300

    Creating Node model and Node struct for API data

---
+++ BipaChallenge/Model/Node.swift
@@ -0,0 +1,20 @@
+//
+//  Node.swift
+//  BipaChallenge
+//
+//  Created by Guilherme Fabbri on 08/01/26.
+//
+
+struct Node {
+        
+    let publicKey: String
+    let alias: String
+    let channels: Int
+    let capacity: Int
+    let firstSeen: Int
+    let updatedAt: Int
+    let city: String?
+    let country: String?
+    
+    
+}
