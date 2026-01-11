d9a3269325fc556b87126ab3c089de97a8022cba
the1scient <guifabbri04@hotmail.com>
2026-01-11 17:13:42 -0300

    Creating ViewModel and Model tests

---
+++ BipaChallengeTests/ModelTests.swift
@@ -0,0 +1,58 @@
+//
+//  ViewModelTests.swift
+//  BipaChallengeUITests
+//
+//  Created by Guilherme Fabbri on 11/01/26.
+//
+
+import XCTest
+@testable import BipaChallenge
+
+final class ModelTests: XCTestCase {
+    
+    func testNodeModelWithFallback() throws {
+        let nodePTBR = Node(
+            publicKey: "", alias: "", channels: 0, capacity: 0, firstSeen: Date(), updatedAt: Date(), city: ["pt-BR": "São Paulo"], country: nil, iso_code: nil
+        )
+        
+        XCTAssertEqual(nodePTBR.cityName, "São Paulo")
+        
+        
+        let nodeUnknown = Node(
+            publicKey: "", alias: "", channels: 0, capacity: 0, firstSeen: Date(), updatedAt: Date(), city: nil, country: nil, iso_code: nil
+        )
+        
+        XCTAssertEqual(nodeUnknown.cityName, "Desconhecida")
+        
+    }
+   
+    
+    func testIsoCountryFlag() throws {
+        
+        let nodeBR = Node(
+            publicKey: "", alias: "", channels: 0, capacity: 0, firstSeen: Date(), updatedAt: Date(), city: nil, country: nil, iso_code: "BR"
+            )
+        
+        XCTAssertEqual(nodeBR.countryFlag, "🇧🇷")
+        
+        let nodeRandom = Node(
+            publicKey: "", alias: "", channels: 0, capacity: 0, firstSeen: Date(), updatedAt: Date(), city: nil, country: nil, iso_code: "ASDSAD"
+            )
+        
+        XCTAssertEqual(nodeRandom.countryFlag, "🌎")
+        
+        let nodeNil =  Node(
+            publicKey: "", alias: "", channels: 0, capacity: 0, firstSeen: Date(), updatedAt: Date(), city: nil, country: nil, iso_code: nil
+            )
+        
+        XCTAssertEqual(nodeNil.countryFlag, "🌎")
+
+        
+        
+    }
+    
+    
+    
+    
+
+}
---
+++ BipaChallengeTests/ViewModelTests.swift
@@ -0,0 +1,83 @@
+//
+//  ViewModelTests.swift
+//  BipaChallengeUITests
+//
+//  Created by Guilherme Fabbri on 11/01/26.
+//
+
+import XCTest
+@testable import BipaChallenge
+
+final class ViewModelTests: XCTestCase {
+    
+    var nVM : NodeViewModel!
+    var cVM : ChannelViewModel!
+    
+
+    override func setUpWithError() throws {
+            nVM = NodeViewModel()
+            cVM = ChannelViewModel()
+    }
+
+    override func tearDownWithError() throws {
+//            nVM = nil
+//            cVM = nil
+    }
+    
+    func testEmptyStates() throws {
+        XCTAssertTrue(nVM.nodes.isEmpty)
+        XCTAssertTrue(cVM.channelLocation.isEmpty)
+        
+        XCTAssertFalse(nVM.isLoading)
+        XCTAssertFalse(cVM.isLoading)
+        
+        XCTAssertNil(nVM.errorMsg)
+        XCTAssertNil(cVM.errorMsg)
+        
+    }
+    
+    func testLoadingState() throws {
+        nVM.fetchNodes()
+        XCTAssertTrue(nVM.isLoading)
+        
+        cVM.fetchChannelGeodata(publicKey: "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f")
+        XCTAssertTrue(cVM.isLoading)
+
+    }
+
+    func testDataReturned() throws {
+        
+        let nodesExpectation = expectation(description: "Successfully fetched Nodes")
+        let channelExpectation = expectation(description: "Successfully fetched Channel's Locations")
+        
+        nVM.fetchNodes()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            XCTAssertFalse(self.nVM.isLoading) // isLoading should already be false
+            XCTAssertGreaterThan(self.nVM.nodes.count, 0) // API should return nodes info successfully and they should be transformed into nodes
+            
+            XCTAssertNil(self.nVM.errorMsg) // no errors should exist
+            nodesExpectation.fulfill()
+        
+            
+            
+        }
+        
+        cVM.fetchChannelGeodata(publicKey: "03864ef025fde8fb587d989186ce6a4a186895ee44a926bfc370e2c366597a3f8f")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            XCTAssertFalse(self.cVM.isLoading)
+            XCTAssertGreaterThan(self.cVM.channelLocation.count, 0)
+            XCTAssertNil(self.cVM.errorMsg)
+            channelExpectation.fulfill()
+        }
+        
+        
+        waitForExpectations(timeout: 20)
+        
+        
+    }
+
+   
+
+}
