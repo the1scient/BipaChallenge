df9c7c1c7eb7acb8be870a99078b91334dcadf33
the1scient <guifabbri04@hotmail.com>
2026-01-14 16:29:50 -0300

    Finishing APIError enum type.

--- BipaChallenge/Networking/APIError.swift
+++ BipaChallenge/Networking/APIError.swift
@@ -9,6 +9,26 @@ import Foundation
 
 enum APIError : Error, LocalizedError {
     
+    case invalidURL
+    case failedRequest
+    case responseInvalid
+    case failedDecoding
+    case noData
     
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "URL inválida"
+        case .failedRequest:
+            return "Falha no Request"
+        case .responseInvalid:
+            return "Resposta da API é inválida!"
+        case .failedDecoding:
+            return "Erro ao fazer decoding"
+        case .noData:
+            return "API não retornou dados"
+        }
+        
+    }
     
 }
