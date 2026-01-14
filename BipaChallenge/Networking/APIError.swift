39fbc970756f7af1496d7491b2ddfd847f94ffc9
the1scient <guifabbri04@hotmail.com>
2026-01-14 16:14:04 -0300

    Creating HTTPMethod enum and APIerror enum for the networking API layer

---
+++ BipaChallenge/Networking/APIError.swift
@@ -0,0 +1,14 @@
+//
+//  APIError.swift
+//  BipaChallenge
+//
+//  Created by Guilherme Fabbri on 14/01/26.
+//
+
+import Foundation
+
+enum APIError : Error, LocalizedError {
+    
+    
+    
+}
---
+++ BipaChallenge/Networking/HTTPMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethod.swift
+//  BipaChallenge
+//
+//  Created by Guilherme Fabbri on 14/01/26.
+//
+
+
+enum HTTPMethod: String {
+    case get
+    case post
+    case put
+    case delete
+}
