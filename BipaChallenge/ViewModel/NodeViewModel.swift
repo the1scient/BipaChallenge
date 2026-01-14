732329bf888ed50224598d9e7d541ee7285a18b8
the1scient <guifabbri04@hotmail.com>
2026-01-14 17:02:45 -0300

    Adding APIClient networking layer to NodeViewModel. Adding .receive, and adding .sink to properly address failure states.

--- BipaChallenge/ViewModel/NodeViewModel.swift
+++ BipaChallenge/ViewModel/NodeViewModel.swift
@@ -13,46 +13,33 @@ final class NodeViewModel : ObservableObject {
     
     @Published var nodes : [Node] = []
     @Published var isLoading : Bool = false
-    @Published var errorMsg : String? = nil
+    @Published var errorMsg : String?
+    
+    let apiClient = APIClient()
+    var cancellable : AnyCancellable?
+    
     
     func fetchNodes() {
         isLoading = true
+        errorMsg = nil
+      
+        cancellable = apiClient.nodes()
+                  .receive(on: DispatchQueue.main)
+                  .sink(
+                      receiveCompletion: { [weak self] completion in
+                          self?.isLoading = false
+                          
+                          if case .failure(let error) = completion {
+                              self?.errorMsg = "\(error)"
+                          }
+                      },
+                      receiveValue: { [weak self] nodes in
+                          self?.nodes = nodes
+                      }
+                  )
+            
+            
         
-        guard let url = URL(string: "https://mempool.space/api/v1/lightning/nodes/rankings/connectivity") else { return }
-        var request = URLRequest(url: url)
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        print("API chamada")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            
-            if let error = error {
-                print("Erro: ", error)
-                self.errorMsg = "\(error)"
-                self.isLoading = false
-                return
-            }
-            
-            guard let data = data else {
-                print("Sem resultados")
-                self.errorMsg = "Nada para exibir..."
-                self.isLoading = false
-                return
-            }
-            
-            do {
-                let nodes = try JSONDecoder().decode([Node].self, from: data)
-                DispatchQueue.main.async {
-                    self.isLoading = false
-                    self.nodes = nodes
-                }
-            }
-            
-            catch {
-                print("Erro ao fazer JSON decode: ", error)
-                self.isLoading = false
-            }
-        }
-        
-        task.resume()
         
     }
     
